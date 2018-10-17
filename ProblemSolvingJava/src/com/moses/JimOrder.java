e84f633da9908bc2cf84ade40dc4df846d696789
moongar <moosa.moghimi@gmail.com>
2018-10-16 22:30:24 -0600

    Completed the solution for the Jim and the Orders challenge

--- ProblemSolvingJava/src/com/moses/JimOrder.java
+++ ProblemSolvingJava/src/com/moses/JimOrder.java
@@ -1,24 +1,51 @@
 package com.moses;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class JimOrder {
     public static void main(String[] args) {
-        int[][] orders={{1,3},{2,3},{3,3}};
+        //int[][] orders={{8,1},{4,2},{5,6},{3,1},{4,3}};
+        int[][] orders={{3,3},{1,2},{2,1},{1,5},{5,1},{4,1},{1,4},{2,3},{3,2}};
         for(int i:jimOrders(orders)){
             System.out.println(i);
         }
     }
     static int[] jimOrders(int[][] orders) {
-        int[] servedFirst= new int[orders.length];
-        int[] serveTime = new int[orders.length];
-        for(int i=0;i<orders.length;i++){
-            serveTime[i]=orders[i][0]+orders[i][1];
+        int n=orders.length;
+        int[] servedFirst= new int[n];
+        List<Student> students=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            students.add(new Student(i+1, orders[i][0],orders[i][1]));
         }
-        Arrays.sort(serveTime);
-
+        Collections.sort(students,Comparator.comparing(Student :: getServingTime).thenComparing(Student::getNumber));
+        for(int i=0;i<n;i++){
+            servedFirst[i]=students.get(i).getNumber();
+        }
         return servedFirst;
+    }
+    static class Student{
+        int number;
+        int orderNumber;
+        int orderTime;
+        int ServingTime;
+
+        public Student(int number, int orderNumber, int orderTime) {
+            this.number = number;
+            this.orderNumber = orderNumber;
+            this.orderTime = orderTime;
+            this.ServingTime=orderNumber+orderTime;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public int getOrderNumber() {
+            return orderNumber;
+        }
+
+        public int getServingTime() {
+            return ServingTime;
+        }
     }
 }
--- ProblemSolvingJava/src/com/moses/Main.java
+++ ProblemSolvingJava/src/com/moses/Main.java
@@ -3,7 +3,6 @@ package com.moses;
 public class Main {
 
     public static void main(String[] args) {
-	// TODO jimOrder has to be fixed
         //TODO Duplicate Words
     }
 }
