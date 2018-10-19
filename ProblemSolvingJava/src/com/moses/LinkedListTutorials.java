f047d416d534741c11e202e01b00e4ec8e8ac10b
moongar <moosa.moghimi@gmail.com>
2018-10-18 19:01:12 -0600

    Added a new class for Linked List Challenges Added solution for Print the Elements of a Linked List challenge

---
+++ ProblemSolvingJava/src/com/moses/LinkedListTutorials.java
@@ -0,0 +1,49 @@
+package com.moses;
+
+public class LinkedListTutorials {
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.insertNode(12);
+        list.insertNode(25);
+        list.insertNode(46);
+        printLinkedList(list.head);
+
+    }
+    static void printLinkedList(SinglyLinkedListNode head) {
+        while ( head != null) {
+            System.out.println(head.data);
+            head = head.next;
+        }
+    }
+}
